25dbab7d52a6a0f2864b02f7ba07e078eac1ed41
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2022-11-18 12:15:12 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,18 @@
 
+var PlacarPlayer = 0;
+var PlacarBot = 0;
+var PlacarEmpates = 0;
+
 function OpcaoPlayer(num) {
     let opcao = document.querySelector("button.botao-individual").value = num
-    if(opcao == 0) {
-       let amarelo = document.querySelectorAll('.botao')[0];
-       let cinza = document.querySelectorAll('.botao')[1];
-       let cinza1 = document.querySelectorAll('.botao')[2];
-       amarelo.style.background = '#ffbd2f'
-       cinza.style.background = '#696969'
-       cinza1.style.background = '#696969'
-        
+    if (opcao == 0) {
+        let amarelo = document.querySelectorAll('.botao')[0];
+        let cinza = document.querySelectorAll('.botao')[1];
+        let cinza1 = document.querySelectorAll('.botao')[2];
+        amarelo.style.background = '#ffbd2f'
+        cinza.style.background = '#696969'
+        cinza1.style.background = '#696969'
+
     } else if (opcao == 1) {
         let amarelo = document.querySelectorAll('.botao')[1];
         let cinza = document.querySelectorAll('.botao')[0];
@@ -28,23 +32,23 @@ function OpcaoPlayer(num) {
 }
 
 function Player() {
-    let opcao = document.querySelector("button.botao-individual").value 
+    let opcao = document.querySelector("button.botao-individual").value
     let value = Number(opcao);
 
-    if(value == 0) {
+    if (value == 0) {
         document.getElementById('player').src = "src/img/pedra.png"
 
-     } else if (value == 1) {
-         document.getElementById('player').src = "src/img/papel.png"
+    } else if (value == 1) {
+        document.getElementById('player').src = "src/img/papel.png"
 
-     } else {
-         document.getElementById('player').src = "src/img/tesoura.png"
-     }
+    } else {
+        document.getElementById('player').src = "src/img/tesoura.png"
+    }
 
-    switch(value) {
+    switch (value) {
         case 0:
             return 'Pedra';
-        case 1: 
+        case 1:
             return 'Papel';
         case 2:
             return 'Tesoura';
@@ -52,29 +56,28 @@ function Player() {
 }
 
 
-function bot(){
+function bot() {
     const sorteador = parseInt(Math.random() * 3);
-    document.getElementById('botJogada').textContent = sorteador;
 
-    if(sorteador == 0) {
+    if (sorteador == 0) {
         document.getElementById('bot').src = "src/img/pedra.png"
 
-     } else if (sorteador == 1) {
-         document.getElementById('bot').src = "src/img/papel.png"
+    } else if (sorteador == 1) {
+        document.getElementById('bot').src = "src/img/papel.png"
 
-     } else {
-         document.getElementById('bot').src = "src/img/tesoura.png"
-     }
+    } else {
+        document.getElementById('bot').src = "src/img/tesoura.png"
+    }
 
-    switch(sorteador){
+    switch (sorteador) {
         case 0:
-           return 'Pedra';
-        case 1: 
-           return 'Papel';
+            return 'Pedra';
+        case 1:
+            return 'Papel';
         case 2:
             return 'Tesoura';
     }
-    
+
 }
 
 
@@ -82,41 +85,55 @@ function resultado() {
 
     var ValorPlayer = Player();
     var valorBot = bot();
-    var result = ''; 
+    var result = '';
 
-    console.log('Bot: ', valorBot);
-    console.log('Player: ', ValorPlayer);
-
-    if(valorBot == 'Pedra') {
-        if(ValorPlayer == 'Tesoura') {
+    if (valorBot == 'Pedra') {
+        if (ValorPlayer == 'Tesoura') {
             result = 'O computador venceu!';
-        } else if(ValorPlayer == 'Papel') {
+            PlacarBot++
+
+        } else if (ValorPlayer == 'Papel') {
             result = 'Parabéns, você ganhou!';
+            PlacarPlayer++
+
         } else {
             result = 'Rodada empatada';
+            PlacarEmpates++
         }
 
     } else if (valorBot == 'Papel') {
 
-        if(ValorPlayer == 'Tesoura') {
+        if (ValorPlayer == 'Tesoura') {
             result = 'Parabéns, você venceu!';
-        } else if(ValorPlayer == 'Papel') {
+            PlacarPlayer++
+
+        } else if (ValorPlayer == 'Papel') {
             result = 'Rodada empatada';
+            PlacarEmpates++
+
         } else {
             result = 'O computador venceu!';
-        } 
+            PlacarBot++
+        }
 
     } else {
-        if(ValorPlayer == 'Tesoura') {
+        if (ValorPlayer == 'Tesoura') {
             result = 'Rodada empatada';
-        } else if(ValorPlayer == 'Papel') {
+            PlacarEmpates++
+
+        } else if (ValorPlayer == 'Papel') {
             result = 'O computador venceu!';
+            PlacarBot++
+
         } else {
             result = 'Parabéns, você venceu!';
-
+            PlacarPlayer++
         }
     }
 
     document.getElementById('resultado').textContent = result;
-
+    var MudarPlacarPlayer = document.querySelector('#placar-player')
+    var MudarPlacarBot = document.querySelector('#placar-bot')
+    MudarPlacarPlayer.textContent = PlacarPlayer;
+    MudarPlacarBot.textContent = PlacarBot;
 } 
